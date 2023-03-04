d44d02c2b639e935dc413041d23898f1a4d377a8
Sebastian Szczepański <contact@sszczep.dev>
2023-03-04 12:50:34 +0100

    Merge `demo` into `development` (#11)
    * feat: Add 3D model
    
    * feat: Update model, improve lightning
    
    * feat: Update model, hide navbar, further adjustments to rendering
    
    * feat: Cleanup code, fix recalculating bbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
+import { Group } from 'three';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Sphere } from '@react-three/drei';
+import { OnCenterCallbackProps, OrbitControls, Stage } from '@react-three/drei';
 
-import useSWRImmutable from 'swr/immutable';
-
-import { Alert, Box, LinearProgress, styled } from '@mui/material';
+import { Box, styled } from '@mui/material';
 
 import type { NextPageWithTitle } from './_app';
 
 import Stats, { StatsContainer } from '../components/home/Stats';
 import AxesHelper from '../components/home/AxesHelper';
 import FullscreenToggle from '../components/home/FullscreenToggle';
+import NodesGroup from '../components/home/NodesGroup';
+import ModelMesh from '../components/home/ModelMesh';
 
 const PageContainer = styled(Box)(({ theme }) => ({
   position: 'relative',
@@ -38,109 +39,49 @@ const CanvasOverlayContainer = styled(Box)(({ theme }) => ({
   },
 }));
 
-type TagPosition = {
-  id: string;
-  x: number;
-  y: number;
-  z: number;
-};
-
-type Tag = {
-  id: string;
-  name: string;
-  positions: Array<TagPosition>;
-};
-
-const sseUrl = new URL(
-  'network/events?deviceTypes=tag&eventTypes=location',
-  process.env.NEXT_PUBLIC_API_BASE_URL || '',
-).href;
-
 const HomePage: NextPageWithTitle = () => {
-  const {
-    data: devices,
-    error,
-    mutate,
-  } = useSWRImmutable<Tag[]>('/devices/tags');
-
-  const onLocationEvent = React.useCallback((event: MessageEvent) => {
-    return;
-  }, []);
-
-  // const onLocationEvent = React.useCallback(
-  //   (event: MessageEvent) => {
-  //     if (!devices) return;
-
-  //     const { deviceType, deviceId, x, y, z } = JSON.parse(
-  //       event.data,
-  //     ) as TagLocationEventData;
-
-  //     const deviceIndex = devices.findIndex(
-  //       (device) =>
-  //         device.deviceType === deviceType && device.deviceId === deviceId,
-  //     );
-
-  //     if (deviceIndex === -1) return;
-
-  //     const newDevicesArray = [
-  //       ...devices.slice(0, deviceIndex),
-  //       {
-  //         ...devices[deviceIndex],
-  //         location: [x, y, z] as [number, number, number],
-  //       },
-  //       ...devices.slice(deviceIndex + 1),
-  //     ];
-
-  //     mutate(newDevicesArray, { revalidate: false });
-  //   },
-  //   [devices, mutate],
-  // );
-
-  React.useEffect(() => {
-    const eventSource = new EventSource(sseUrl);
-    eventSource.addEventListener('location', onLocationEvent);
-
-    return () => {
-      eventSource.removeEventListener('location', onLocationEvent);
-      eventSource.close();
-    };
-  }, [onLocationEvent]);
-
   const pageContainerRef = React.useRef<HTMLElement>(null);
   const statsContainerRef = React.useRef<HTMLElement>(null);
+  const nodesGroupRef = React.useRef<Group>(null);
 
-  if (!devices && error) {
-    return (
-      <Alert severity="error">Could not fetch network&apos;s devices</Alert>
-    );
-  }
-
-  if (!devices) {
-    return <LinearProgress />;
-  }
+  const onStageCentered = React.useCallback((props: OnCenterCallbackProps) => {
+    const { x, y, z } = props.center;
+    nodesGroupRef.current?.position.set(-x, -y, -z);
+  }, []);
 
   return (
     <PageContainer ref={pageContainerRef}>
       <CanvasContainer>
-        <Canvas camera={{ position: [5, 5, 5], fov: 50 }} frameloop="demand">
+        <Canvas
+          camera={{ position: [5, 5, 5], fov: 50 }}
+          frameloop="demand"
+          shadows
+        >
           <OrbitControls makeDefault />
           <Stats parent={statsContainerRef} />
           <AxesHelper />
           <gridHelper args={[100, 100]} />
 
-          {devices.map((device) => (
-            <Sphere
-              key={device.id}
-              position={[
-                device.positions[0].x,
-                device.positions[0].y,
-                device.positions[0].z,
-              ]}
-              args={[0.1]}
-            >
-              <meshBasicMaterial color="hotpink" />
-            </Sphere>
-          ))}
+          <NodesGroup ref={nodesGroupRef} />
+
+          <Stage
+            shadows={{
+              type: 'contact',
+              size: 2048,
+            }}
+            intensity={0.5}
+            environment="apartment"
+            preset={{
+              main: [1, 3, 1],
+              fill: [-2, -0.5, -2],
+            }}
+            adjustCamera={false}
+            center={{
+              onCentered: onStageCentered,
+            }}
+          >
+            <ModelMesh />
+          </Stage>
         </Canvas>
       </CanvasContainer>
 
